e6903b9b068568b97e5afc0d5686f0b0faa5cbc0
Accomplice <ywang315@ucsc.edu>
2022-06-08 14:02:21 +0100

    Update

--- Assets/Scripts/YiboTest/MapManager.cs
+++ Assets/Scripts/YiboTest/MapManager.cs
@@ -13,31 +13,39 @@ public class MapManager : MonoBehaviour
     private Vector3 playerPos;
     private int[] currentPosNum;
     private int currentQuadrant;
+    private int[] previousPosNum;
     // Start is called before the first frame update
     void Start()
     {
+
+
         maps = new GameObject[3, 3];
         int mapImportNum = 0;
-        for(int i = 0; i < 3; i++)
+        for (int i = 0; i < 3; i++)
         {
-            for(int j = 0; j < 3; j++)
+            for (int j = 0; j < 3; j++)
             {
-                maps[i,j] = importMaps[mapImportNum];
+                maps[i, j] = importMaps[mapImportNum];
                 mapImportNum++;
             }
         }
-        playerPos = new Vector3(player.transform.position.x, player.transform.position.y,0);
+        playerPos = new Vector3(player.transform.position.x, player.transform.position.y, 0);
 
-        for(int i = 0; i < importMaps.Length; i++)
+        for (int i = 0; i < importMaps.Length; i++)
         {
-           importMaps[i].SetActive(false);
+            //importMaps[i].SetActive(false);
+            //Set all maps to deactive
         }
+
+        currentPosNum = this.getLocatingMapNum();
+        previousPosNum = currentPosNum;
+        //inject pos data
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void FixedUpdate()
@@ -46,35 +54,49 @@ public class MapManager : MonoBehaviour
         currentPosNum = this.getLocatingMapNum();
         currentQuadrant = this.getQuadrant();
 
-            //this.triggerMap();
-        
-        Debug.Log(currentPosNum[0] + " " + currentPosNum[1]);
-        Debug.Log(currentQuadrant);
+        this.triggerMap();
+
+
+        if(currentPosNum[0] != previousPosNum[0] || currentPosNum[1] != previousPosNum[1]) 
+        {
+            this.moveMap();
+            Debug.Log("c" + currentPosNum[0] + " " + currentPosNum[1]);
+            Debug.Log("p" + previousPosNum[0] + " " + previousPosNum[1]);
+            Debug.Log("change");
+            previousPosNum = currentPosNum;
+            
+        }
+
+        //debug
+        //Debug.Log(currentPosNum[0] + " " + currentPosNum[1]);
+        //Debug.Log(currentQuadrant);
     }
 
-    private int[] getLocatingMapNum() 
+    private int[] getLocatingMapNum()
     {
-        int[] locatingMapNum  = {1,1};
+        int[] locatingMapNum = { 1, 1 };
         for (int i = 0; i < 3; i++)
         {
             for (int j = 0; j < 3; j++)
             {
+               
                 Vector3 mapPos = maps[i, j].transform.position;
-                if ((Mathf.Abs(mapPos.x - playerPos.x) < mapSize/2) && (Mathf.Abs(mapPos.y - playerPos.y) < mapSize / 2))
+                if ((Mathf.Abs(mapPos.x - playerPos.x) < mapSize / 2) && (Mathf.Abs(mapPos.y - playerPos.y) < mapSize / 2))
                 {
-                    locatingMapNum = new int[]{i,j};
+                    locatingMapNum = new int[] { i, j };
+                    return locatingMapNum;
                 }
             }
         }
-        return locatingMapNum;
+        return previousPosNum;
     }
 
-    private int getQuadrant() 
+    private int getQuadrant()
     {
         //  1|2
         //  3|4
         int quadrantNum = 1;
-        GameObject tempMap = maps[currentPosNum[0],currentPosNum[1]];
+        GameObject tempMap = maps[currentPosNum[0], currentPosNum[1]];
         if ((tempMap.transform.position.y - playerPos.y) < 0)
         {
             // if the player pos is on the upper quadrant
@@ -82,20 +104,20 @@ public class MapManager : MonoBehaviour
             {
                 quadrantNum = 1;
             }
-            else 
+            else
             {
                 quadrantNum = 2;
             }
 
         }
-        else 
+        else
         {
             //player pos is on the lower quadrant
             if ((tempMap.transform.position.x - playerPos.x) > 0)
             {
                 quadrantNum = 3;
             }
-            else 
+            else
             {
                 quadrantNum = 4;
             }
@@ -103,33 +125,102 @@ public class MapManager : MonoBehaviour
         return quadrantNum;
     }
 
-    private void triggerMap() 
+    private void triggerMap()
     {
         GameObject tempMap = maps[currentPosNum[0], currentPosNum[1]];
         tempMap.SetActive(true);
-        if (currentQuadrant == 1) 
+        int left = currentPosNum[1] - 1, right = currentPosNum[1] + 1, up = currentPosNum[0] + 1, down = currentPosNum[0] - 1;
+        if (left == -1)
+            left = 2;
+        if (right == 3)
+            right = 0;
+        if (up == 3)
+            up = 0;
+        if (down == -1)
+            down = 2;
+        if (currentQuadrant == 1)
         {
-            maps[currentPosNum[0] + 1, currentPosNum[1] - 1].SetActive(true);
-            maps[currentPosNum[0], currentPosNum[1] - 1].SetActive(true);
-            maps[currentPosNum[0] + 1, currentPosNum[1]].SetActive(true);
+            maps[up, left].SetActive(true);
+            maps[currentPosNum[0], left].SetActive(true);
+            maps[up, currentPosNum[1]].SetActive(true);
         }
-        else if (currentQuadrant == 2) 
+        else if (currentQuadrant == 2)
         {
-            maps[currentPosNum[0] + 1, currentPosNum[1] + 1].SetActive(true);
-            maps[currentPosNum[0], currentPosNum[1] + 1].SetActive(true);
-            maps[currentPosNum[0] + 1, currentPosNum[1]].SetActive(true);
+            maps[up, right].SetActive(true);
+            maps[currentPosNum[0], right].SetActive(true);
+            maps[up, currentPosNum[1]].SetActive(true);
         }
-        else if (currentQuadrant == 3) 
+        else if (currentQuadrant == 3)
         {
-            maps[currentPosNum[0] - 1, currentPosNum[1] - 1].SetActive(true);
-            maps[currentPosNum[0], currentPosNum[1] - 1].SetActive(true);
-            maps[currentPosNum[0] - 1, currentPosNum[1]].SetActive(true);
+            maps[down, left].SetActive(true);
+            maps[currentPosNum[0], left].SetActive(true);
+            maps[down, currentPosNum[1]].SetActive(true);
         }
-        else 
+        else
         {
-            maps[currentPosNum[0] - 1, currentPosNum[1] + 1].SetActive(true);
-            maps[currentPosNum[0], currentPosNum[1] + 1].SetActive(true);
-            maps[currentPosNum[0] - 1, currentPosNum[1]].SetActive(true);
+            maps[down, right].SetActive(true);
+            maps[currentPosNum[0], right].SetActive(true);
+            maps[down, currentPosNum[1]].SetActive(true);
         }
     }
+
+    private void moveMap() 
+    {
+        GameObject tempCMap = maps[currentPosNum[0], currentPosNum[1]];
+        GameObject tempPMap = maps[previousPosNum[0], previousPosNum[1]];
+
+            if (tempCMap.transform.position.y > tempPMap.transform.position.y) 
+            {
+                Debug.Log("went up");
+                //went up
+                for (int i = 0; i < 3; i++) 
+                {
+                    maps[previousPosNum[1]-1,i].transform.position = new Vector3( maps[previousPosNum[1]-1, i].transform.position.x, 
+                                                                                    maps[previousPosNum[1]-1, i].transform.position.y + (3 * mapSize), 
+                                                                                    maps[previousPosNum[1]-1, i].transform.position.z);
+                    //maps[i, currentPosNum[1] - 1].transform.position.y += 2 * mapSize;
+                }
+            }
+            else if (tempCMap.transform.position.y < tempPMap.transform.position.y)
+            {
+                Debug.Log("went down");
+                for (int i = 0; i < 3; i++)
+                {
+                    maps[previousPosNum[1]-1, i].transform.position = new Vector3( maps[previousPosNum[1]-1, i].transform.position.x, 
+                                                                                    maps[previousPosNum[1]-1, i].transform.position.y - (3 * mapSize), 
+                                                                                    maps[previousPosNum[1]-1, i].transform.position.z);
+                    //maps[i, currentPosNum[1] + 1].transform.position.y -= 2 * mapSize;
+                }
+                //went down
+            }
+       
+
+            if (tempCMap.transform.position.x > tempPMap.transform.position.x)
+            {
+                Debug.Log("went right");
+                for (int i = 0; i < 3; i++)
+                {
+                    maps[i, previousPosNum[0]].transform.position = new Vector3( maps[i, previousPosNum[0]].transform.position.x + (3 * mapSize), 
+                                                                                    maps[i, previousPosNum[0]].transform.position.y, 
+                                                                                    maps[i, previousPosNum[0]].transform.position.z);
+                    //maps [currentPosNum[1] - 1 ,i].transform.position.x += 2 * mapSize;
+                }
+                //went right
+            }
+            else if (tempCMap.transform.position.x < tempPMap.transform.position.x)
+            {
+                Debug.Log("went left");
+                for (int i = 0; i < 3; i++)
+                {
+                    maps[i, previousPosNum[0]].transform.position = new Vector3( maps[i, previousPosNum[0]].transform.position.x - (3 * mapSize), 
+                                                                                    maps[i, previousPosNum[0]].transform.position.y, 
+                                                                                    maps[i, previousPosNum[0]].transform.position.z);
+                    //maps[currentPosNum[1] + 1, i].transform.position.x -= 2 * mapSize;
+                }
+                //went left
+            }
+        
+        
+    }
+
 }

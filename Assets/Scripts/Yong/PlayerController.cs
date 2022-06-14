c90347be5d69e80efccfbf129637652b667ee590
hahaYong <38389978+woshifeiyang@users.noreply.github.com>
2022-06-14 22:07:34 +0100

    Enemy detecter

--- Assets/Scripts/Yong/PlayerController.cs
+++ Assets/Scripts/Yong/PlayerController.cs
@@ -9,7 +9,13 @@ public class PlayerController : MonoBehaviour
 
     private Animator _anim;
     
+    private CircleCollider2D _cc;
+    
     private Vector2 _movement;
+
+    private GameObject _nearestEnemy;
+
+    private bool _hasFound;
 
     public Transform skill;
 
@@ -19,7 +25,11 @@ public class PlayerController : MonoBehaviour
     {
         _rb = GetComponent<Rigidbody2D>();
         _anim = GetComponent<Animator>();
+        _cc = GetComponent<CircleCollider2D>();
+
+        _hasFound = false;
         InvokeRepeating("SpawnSkill", 1.0f, 3.0f);
+        StartCoroutine("FindNearestTarget");
     }
 
     // Update is called once per frame
@@ -38,6 +48,10 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         _rb.MovePosition(_rb.position + _movement * speed * Time.fixedDeltaTime);
+        if (_nearestEnemy)
+        {
+            Debug.DrawLine(transform.position, _nearestEnemy.transform.position, Color.red);
+        }
     }
 
     private void SwitchAnim()
@@ -47,7 +61,30 @@ public class PlayerController : MonoBehaviour
 
     void SpawnSkill()
     {
-        
         Instantiate(skill, new Vector3(transform.position.x, transform.position.y + 1.0f), transform.rotation);
+    }
+
+    IEnumerator FindNearestTarget()
+    {
+        float radius = _cc.radius;
+        while (true)
+        {
+            yield return new WaitForSeconds(0.05f);
+            if (_hasFound)
+            {
+                _cc.radius = radius;
+                _hasFound = false;
+            }
+            else _cc.radius += 1.0f;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            _nearestEnemy = other.gameObject;
+            _hasFound = true;
+        }
     }
 }

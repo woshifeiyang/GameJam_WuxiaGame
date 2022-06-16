fc5d99cbeade1a146ac2495d3581f30afc6194cb
Accomplice <50436515+myrphigh@users.noreply.github.com>
2022-06-16 11:21:07 +0100

    change enemy rb

--- Assets/Scripts/Yong/Monster.cs
+++ Assets/Scripts/Yong/Monster.cs
@@ -20,6 +20,7 @@ public class Monster : MonoBehaviour
         _rb = GetComponent<Rigidbody2D>();
         _anim = GetComponent<Animator>();
         _player = GameObject.FindGameObjectWithTag("Player");
+        
     }
 
     // Update is called once per frame
@@ -29,6 +30,7 @@ public class Monster : MonoBehaviour
     }
     private void FixedUpdate()
     {
+        //rb.AddForce((_player.transform.position - transform.position).normalized * Time.fixedDeltaTime * moveSpeed, ForceMode2D.Force);
         _rb.MovePosition(transform.position + (_player.transform.position - transform.position).normalized * Time.fixedDeltaTime * moveSpeed);
         if (transform.position.x - _player.transform.position.x > 0)
         {

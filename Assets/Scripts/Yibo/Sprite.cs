2a24d6e3bba9b140c3036a388c58593d4939759d
Accomplice <50436515+myrphigh@users.noreply.github.com>
2022-06-16 15:39:59 +0100

    sprite update save

---
+++ Assets/Scripts/Yibo/Sprite.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sprite : MonoBehaviour
+{
+    //define sprite dictionary
+    public Dictionary<String, float> spriteProperty = new Dictionary<string, float>();
+}

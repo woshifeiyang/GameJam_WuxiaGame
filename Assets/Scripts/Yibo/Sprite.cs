bd942e7e7a739b5ab13a8c8dbcd35de80d191deb
Accomplice <50436515+myrphigh@users.noreply.github.com>
2022-06-16 17:09:45 +0100

    update sprite

--- Assets/Scripts/Yibo/Sprite.cs
+++ Assets/Scripts/Yibo/Sprite.cs
@@ -3,8 +3,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Sprite : MonoBehaviour
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/SpawnManagerScriptableObject", order = 1)]
+public class Sprite :  ScriptableObject
 {
+    public string spriteName;
+
+    public float damageFactor;
+    public float projectileSize;
+    public float projectileNum;
+    public float coolDownReduce;
+    public float healthBonus;
+    public float damageReduction;
+    public float movingSpeedBonus;
+    public float currencyBonusFactor;
+    public float shieldNumAdd;
+    public float shieldCooldownReduction;
+    public float respawnChanceBonus;
+    public float luckinessFactor;
+    public float killCountFactor;
+    public float bossDamageFactor;
+    public float healthStealthFactor;
+    public float itemBonus;
+    
     //define sprite dictionary
-    public Dictionary<String, float> spriteProperty = new Dictionary<string, float>();
+    public Dictionary<string, float> spriteProperty = new Dictionary<string, float>();
+
+    private void Awake()
+    {
+        AddNums();
+        AddNewSprite();
+    }
+
+    public void AddNewSprite()
+    {
+        SpriteManager spriteManager = GameObject.FindObjectOfType<SpriteManager>();
+        spriteManager.AddNewSprite(spriteName, this);
+    }
+
+    private void AddNums()
+    {
+        spriteProperty.Add("damageFactor", damageFactor);
+        spriteProperty.Add("projectileSize", projectileSize);
+        spriteProperty.Add("projectileNum", projectileNum);
+        spriteProperty.Add("coolDownReduce", coolDownReduce);
+        spriteProperty.Add("healthBonus", healthBonus);
+        spriteProperty.Add("damageReduction", damageReduction);
+        spriteProperty.Add("movingSpeedBonus", movingSpeedBonus);
+        spriteProperty.Add("currencyBonusFactor", currencyBonusFactor);
+        spriteProperty.Add("shieldNumAdd", shieldNumAdd);
+        spriteProperty.Add("shieldCooldownReduction", shieldCooldownReduction);
+        spriteProperty.Add("respawnChanceBonus", respawnChanceBonus);
+        spriteProperty.Add("luckinessFactor", luckinessFactor);
+        spriteProperty.Add("killCountFactor", killCountFactor);
+        spriteProperty.Add("bossDamageFactor", bossDamageFactor);
+        spriteProperty.Add("healthStealthFactor", healthStealthFactor);
+        spriteProperty.Add("itemBonus", itemBonus);
+    }
 }
--- Assets/Scripts/Yibo/SpriteManager.cs
+++ Assets/Scripts/Yibo/SpriteManager.cs
@@ -4,5 +4,34 @@ using UnityEngine;
 
 public class SpriteManager : Singleton<SpriteManager>
 {
+    public Dictionary<string, Sprite> spriteLibrary = new Dictionary<string, Sprite>();
+    
+    public void AddNewSprite(string spriteName, Sprite targetSprite)
+    {
+        // add new sprite entity to the library
+        // KEY is spriteName
+        // VALUE is targetSprite
+        if (!spriteLibrary.ContainsKey(spriteName))
+        {
+            spriteLibrary.Add(spriteName, targetSprite);
+        }
+        else
+        {
+            spriteLibrary[spriteName] = targetSprite;
+        }
+    }
+
+    public void DeleteSprite(string spriteName)
+    {
+        //detele value with selected name
+        if(spriteLibrary.ContainsKey(spriteName))
+        spriteLibrary.Remove(spriteName);
+    }
+
+    public Sprite GetSprite(string spriteName)
+    {
+        return spriteLibrary[spriteName];
+    }
+    
     
 }

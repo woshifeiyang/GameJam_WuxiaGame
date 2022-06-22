9f26bd5210ca9e6566a982d725f6a2270c9d2bd9
Accomplice <50436515+myrphigh@users.noreply.github.com>
2022-06-22 16:41:16 +0100

    show fps in ui

--- Assets/Scripts/Yibo/showFPS.cs
+++ Assets/Scripts/Yibo/showFPS.cs
@@ -5,10 +5,18 @@ using UnityEngine.UI;
 public class showFPS : MonoBehaviour {
     public Text fpsText;
     public float deltaTime;
- 
+
+    public float timeGap = 0.5f;
+    private float tempTime;
     void Update () {
         deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
         float fps = 1.0f / deltaTime;
-        fpsText.text = Mathf.Ceil (fps).ToString ();
+
+        tempTime += Time.deltaTime;
+        if (tempTime > timeGap)
+        {
+            fpsText.text = Mathf.Ceil(fps).ToString();
+            tempTime = 0f;
+        }
     }
 }

ff35aad019a7e07ad8fc7ef2b7e4053db0521045
Yong <38389978+woshifeiyang@users.noreply.github.com>
2022-06-27 13:25:18 +0100

    Value and GiftUI change

--- Assets/Scripts/Yong/UIManager.cs
+++ Assets/Scripts/Yong/UIManager.cs
@@ -7,9 +7,11 @@ public class UIManager : MonoBehaviour
 {
     public static UIManager UIManagerInstance;
 
-    private GameObject rogueUIGameObject;
+    private GameObject _rogueUIObj;
+    
+    private GameObject _giftUIObj;
 
-    private GameObject phoneButtonObject;
+    private GameObject _phoneButtonObj;
 
     private void Awake()
     {
@@ -19,26 +21,14 @@ public class UIManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if (GameObject.Find("RogueUI"))
-        {
-            rogueUIGameObject = GameObject.Find("RogueUI");
-            rogueUIGameObject.SetActive(false);
-        }
-        else
-        {
-            Debug.Log("Can not find RogueUI, please check if the name of object is right");
-        }
-
-        if (GameObject.Find("PhoneButton"))
-        {
-            phoneButtonObject = GameObject.Find("PhoneButton");
-            phoneButtonObject.SetActive(true);
-        }
-        else
-        {
-            Debug.Log("Can not find PhoneButton, please check if the name of object is right");
-        }
+        _rogueUIObj = GameObject.Find("RogueUI");
+        _rogueUIObj.SetActive(false);
         
+        _phoneButtonObj = GameObject.Find("PhoneButton");
+        _phoneButtonObj.SetActive(true);
+
+        _giftUIObj = GameObject.Find("GiftUI");
+        _giftUIObj.SetActive(false);
     }
 
     // Update is called once per frame
@@ -78,9 +68,17 @@ public class UIManager : MonoBehaviour
 
     public void ShowRogueUI()
     {
-        rogueUIGameObject.SetActive(true);
-        rogueUIGameObject.GetComponent<Animator>().SetBool("isVisable", true);
-        phoneButtonObject.SetActive(false);
+        _rogueUIObj.SetActive(true);
+        _rogueUIObj.GetComponent<Animator>().SetBool("isVisable", true);
+        _phoneButtonObj.SetActive(false);
+        PauseGame();
+    }
+
+    public void ShowGiftUI()
+    {
+        _giftUIObj.SetActive(true);
+        _giftUIObj.GetComponent<Animator>().SetBool("isVisable", true);
+        _phoneButtonObj.SetActive(false);
         PauseGame();
     }
 
@@ -89,6 +87,11 @@ public class UIManager : MonoBehaviour
         StartCoroutine(CloseRogueUI_C());
     }
 
+    public void CloseGiftUI()
+    {
+        StartCoroutine(CloseGiftUI_C());
+    }
+    
     public void ChangeScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
@@ -96,11 +99,21 @@ public class UIManager : MonoBehaviour
 
     IEnumerator CloseRogueUI_C()
     {
-        rogueUIGameObject.GetComponent<Animator>().SetBool("isVisable", false);
-        phoneButtonObject.SetActive(true);
+        _rogueUIObj.GetComponent<Animator>().SetBool("isVisable", false);
+        _phoneButtonObj.SetActive(true);
         RestartGame();
         yield return new WaitForSeconds(0.5f);
-        rogueUIGameObject.SetActive(false);
+        _rogueUIObj.SetActive(false);
         StopCoroutine(nameof(CloseRogueUI_C));
+    }
+    
+    IEnumerator CloseGiftUI_C()
+    {
+        _giftUIObj.GetComponent<Animator>().SetBool("isVisable", false);
+        _phoneButtonObj.SetActive(true);
+        RestartGame();
+        yield return new WaitForSeconds(0.5f);
+        _giftUIObj.SetActive(false);
+        StopCoroutine(nameof(CloseGiftUI_C));
     }
 }

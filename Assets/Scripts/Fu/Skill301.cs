97404d4111ffd2a628f48f3671b4187b964aea81
LeadBet <wuzhirick@gmail.com>
2022-07-05 23:17:43 +0800

    skill 301 303

--- Assets/Scripts/Fu/Skill301.cs
+++ Assets/Scripts/Fu/Skill301.cs
@@ -7,6 +7,8 @@ public class Skill301 : FieldSkillBase
     [SerializeField]
     private Animator animator;
     private float skill301Timer;
+    [SerializeField]
+    private float Skill301LastSeconds;
     public override void Start()
     {
         this.GetComponent<CircleCollider2D>().radius = range;
@@ -23,9 +25,17 @@ public class Skill301 : FieldSkillBase
             skill301Timer = 0;
 
             this.GetComponent<CircleCollider2D>().enabled = !this.GetComponent<CircleCollider2D>().enabled;
-            this.GetComponent<CircleCollider2D>().enabled = !this.GetComponent<CircleCollider2D>().enabled;
+
+            StartCoroutine("SkillLastSeconds");
+
 
         }
 
+    }
+
+    IEnumerator SkillLastSeconds()
+    {
+        yield return new WaitForSeconds(Skill301LastSeconds);
+        this.GetComponent<CircleCollider2D>().enabled = !this.GetComponent<CircleCollider2D>().enabled;
     }
 }
--- Assets/Scripts/Fu/Skill303.cs
+++ Assets/Scripts/Fu/Skill303.cs
@@ -9,8 +9,10 @@ public class Skill303 : FieldSkillBase
     [SerializeField]
     private float skillRadius;
     private float skill303Timer;
+    [SerializeField]
+    private float skill303LastSeconds;
 
-    [Header ("Prefab Place")]
+    [Header("Prefab Place")]
     [SerializeField]
     private GameObject circleLeft;
     [SerializeField]
@@ -20,34 +22,46 @@ public class Skill303 : FieldSkillBase
     [SerializeField]
     private GameObject circleBottom;
 
-    private Vector3 playerPosition; 
+    private GameObject circleLeftGameobject;
+    private GameObject circleRightGameobject;
+    private GameObject circleTopGameobject;
+    private GameObject circleBottomGameobject;
+
+    private Vector3 playerPosition;
     // Start is called before the first frame update
-    void Start()
+    public override void Start()
     {
-        List<GameObject> circleList = new List<GameObject>() {circleLeft, circleRight, circleTop, circleBottom};
+        List<GameObject> circleList = new List<GameObject>() { circleLeft, circleRight, circleTop, circleBottom };
     }
 
     // Update is called once per frame
-    void Update()
+    public override void Update()
     {
         playerPosition = PlayerController.Instance.GetPlayerPosition();
         skill303Timer += Time.deltaTime;
         if (skill303Timer > cd)
         {
             skill303Timer = 0;
-            Instantiate(circleLeft, new Vector2((playerPosition.x - skillRadius), playerPosition.y), Quaternion.identity);
+            circleLeftGameobject = Instantiate(circleLeft, new Vector2((playerPosition.x - skillRadius), playerPosition.y), Quaternion.identity).gameObject;
             circleLeft.GetComponent<CircleCollider2D>().radius = range;
-            Instantiate(circleRight, new Vector2((playerPosition.x + skillRadius), playerPosition.y), Quaternion.identity);
+            circleRightGameobject = Instantiate(circleRight, new Vector2((playerPosition.x + skillRadius), playerPosition.y), Quaternion.identity);
             circleRight.GetComponent<CircleCollider2D>().radius = range;
-            Instantiate(circleTop, new Vector2(playerPosition.x, (playerPosition.y + skillRadius)), Quaternion.identity);
+            circleTopGameobject = Instantiate(circleTop, new Vector2(playerPosition.x, (playerPosition.y + skillRadius)), Quaternion.identity);
             circleTop.GetComponent<CircleCollider2D>().radius = range;
-            Instantiate(circleTop, new Vector2(playerPosition.x, (playerPosition.y - skillRadius)), Quaternion.identity);
+            circleBottomGameobject = Instantiate(circleTop, new Vector2(playerPosition.x, (playerPosition.y - skillRadius)), Quaternion.identity);
             circleBottom.GetComponent<CircleCollider2D>().radius = range;
-            Destroy(circleLeft);
-            Destroy(circleRight);
-            Destroy(circleTop);
-            Destroy(circleBottom);
+            StartCoroutine("SkillLastSeconds");
+
         }
 
+    }
+
+    IEnumerator SkillLastSeconds()
+    {
+        yield return new WaitForSeconds(skill303LastSeconds);
+        DestroyImmediate(circleLeftGameobject, true);
+        DestroyImmediate(circleRightGameobject, true);
+        DestroyImmediate(circleTopGameobject, true);
+        DestroyImmediate(circleBottomGameobject, true);
     }
 }

9244421953ef385ceaaf16a90ae6707301ffbc5b
FengYMa <190959996@qq.com>
2022-07-06 15:09:13 +0100

    skill_sword

--- Assets/Scripts/YF/Skill102.cs
+++ Assets/Scripts/YF/Skill102.cs
@@ -13,19 +13,23 @@ public class Skill102 : BulletSkillBase
     public override void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-        Vector3 v = EnemyDetector.Instance.GetNearestEnemyLoc() - PlayerController.Instance.transform.position;
-        v.z = 0;
-        float angle = Vector3.SignedAngle(Vector3.up, v, Vector3.forward);
+        Vector3 Dir = EnemyDetector.Instance.GetNearestEnemyLoc() - PlayerController.Instance.transform.position;
+        Quaternion pianyi = Quaternion.Euler(0, 0, -30);
+        Vector3 Normal = pianyi * Dir;
+        Normal.z = 0;
+        float angle = Vector3.SignedAngle(Vector3.up, Normal, Vector3.forward);
         Quaternion rotation = Quaternion.Euler(0, 0, angle);
         transform.rotation = rotation;
-        _rb.AddForce((EnemyDetector.Instance.GetNearestEnemyLoc() - PlayerController.Instance.transform.position).normalized * speed, ForceMode2D.Force);
+        _rb.AddForce((Normal).normalized * speed, ForceMode2D.Force);
+        //_rb.velocity = current * 0.02f;
         // 持续时间结束时销毁自身
         Invoke("SelfDestory", skillTime);
+
     }
     // Update is called once per frame
     public override void Update()
     {
-        
+
     }
 
     private void SelfDestory()
--- Assets/Scripts/YF/Skill103.cs
+++ Assets/Scripts/YF/Skill103.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
 
-public class Skill101 : BulletSkillBase
+public class Skill103 : BulletSkillBase
 {
     private Rigidbody2D _rb;
 
@@ -13,19 +13,22 @@ public class Skill101 : BulletSkillBase
     public override void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-        Vector3 v = EnemyDetector.Instance.GetNearestEnemyLoc() - PlayerController.Instance.transform.position;
-        v.z = 0;
-        float angle = Vector3.SignedAngle(Vector3.up, v, Vector3.forward);
+        Vector3 Dir = EnemyDetector.Instance.GetNearestEnemyLoc() - PlayerController.Instance.transform.position;
+        Quaternion pianyi = Quaternion.Euler(0, 0, 30);
+        Vector3 Normal = pianyi * Dir;
+        Normal.z = 0;
+        float angle = Vector3.SignedAngle(Vector3.up, Normal, Vector3.forward);
         Quaternion rotation = Quaternion.Euler(0, 0, angle);
         transform.rotation = rotation;
-        _rb.AddForce(v.normalized * speed, ForceMode2D.Force);
+        _rb.AddForce((Normal).normalized * speed, ForceMode2D.Force);
+        //_rb.velocity = current * 0.02f;
         // 持续时间结束时销毁自身
         Invoke("SelfDestory", skillTime);
     }
     // Update is called once per frame
     public override void Update()
     {
-        
+
     }
 
     private void SelfDestory()

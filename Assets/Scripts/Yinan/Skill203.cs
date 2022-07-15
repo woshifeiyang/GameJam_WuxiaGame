9e8ed7a1517e9f0725ab158c546fc04952e0ca05
FengYMa <190959996@qq.com>
2022-07-15 14:45:41 +0100

    New_UI

--- Assets/Scripts/Yinan/Skill203.cs
+++ Assets/Scripts/Yinan/Skill203.cs
@@ -4,23 +4,14 @@ using UnityEngine;
 
 public class Skill203 : FieldSkillBase
 {
-    private GameObject obj = null;
-    public void Awake()
-    {
-        obj = GameObject.Find("Player");
-    }
+    
     
     public override void Start()
     {
-         obj.AddComponent<AntiThunder>();
-        //SkillObj = Resources.Load("Prefab/201") as GameObject;
-        //UpgradeSkill();
+        //GameObject.Find("Player").AddComponent<AntiThunder>(); ;
+   
     }
-    public override void Update()
-    {
-        base.Update();
-        
-    }
+    
 
     
 

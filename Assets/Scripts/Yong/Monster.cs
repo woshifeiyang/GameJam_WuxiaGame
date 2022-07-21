4ab63ebc186d8a979031c97848ea39f5fc5e1757
Lexaria <44296283+Lexaria@users.noreply.github.com>
2022-07-21 17:17:06 +0100

    Elite Enemy Outline

--- Assets/Scripts/Yong/Monster.cs
+++ Assets/Scripts/Yong/Monster.cs
@@ -202,7 +202,7 @@ public class Monster : MonoBehaviour
     {
         _material.EnableKeyword("_BEATTACK");
 
-            yield return new WaitForSeconds(0.1f);
+        yield return new WaitForSeconds(0.1f);
         
         _material.DisableKeyword("_BEATTACK");
 

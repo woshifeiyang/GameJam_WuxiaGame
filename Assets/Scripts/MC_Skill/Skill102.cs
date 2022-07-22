587d3f9fcf0015cff8932af0d2444ff74f5ac0b5
hahaYong <38389978+woshifeiyang@users.noreply.github.com>
2022-07-23 00:59:15 +0100

    Bullet skill num function finish

--- Assets/Scripts/MC_Skill/Skill102.cs
+++ Assets/Scripts/MC_Skill/Skill102.cs
@@ -8,6 +8,10 @@ using UnityEngine.Serialization;
 public class Skill102 : BulletSkillBase
 {
     private Rigidbody2D _rb;
+    
+    private Quaternion _rotation;
+
+    private Vector3 _vec;
 
     // Start is called before the first frame update
     public override void Start()
@@ -15,14 +19,25 @@ public class Skill102 : BulletSkillBase
         //伤害系数
         damage *= 3f;
         _rb = GetComponent<Rigidbody2D>();
-        Vector3 vec = EnemyDetector.Instance.GetNearestEnemyLoc() - PlayerController.Instance.transform.position;
-        vec.z = 0;
-        float angle = Vector3.SignedAngle(Vector3.up, vec, Vector3.forward);
+        _vec = EnemyDetector.Instance.GetNearestEnemyLoc() - PlayerController.Instance.transform.position;
+        float angle = Vector3.SignedAngle(Vector3.up, _vec, Vector3.forward);
         Quaternion rotation = Quaternion.Euler(0, 0, angle);
-        transform.rotation = rotation;
-        _rb.AddForce(vec.normalized * speed, ForceMode2D.Force);
+        _rotation = rotation;
+        transform.rotation = _rotation;
+        _rb.AddForce(_vec.normalized * speed, ForceMode2D.Force);
         // 持续时间结束时销毁自身
         Invoke("SelfDestory", skillTime);
+        
+        string assertPath = "Prefab/Skill/Bullet/102m";
+        for (int i = 1; i < skillNum; i++)
+        {
+            float offset = i % 2 == 0 ? (i + 1) / 2 : -1 * ((i + 1) / 2);
+            GameObject childSkill = (GameObject)Instantiate(Resources.Load(assertPath));
+            
+            childSkill.transform.position = new Vector3(transform.position.x + offset, transform.position.y + offset, 0);
+            childSkill.transform.rotation = _rotation;
+            childSkill.GetComponent<Rigidbody2D>().AddForce(_vec.normalized * speed, ForceMode2D.Force);
+        }
     }
     // Update is called once per frame
     public override void Update()
---
+++ Assets/Scripts/MC_Skill/Skill102M.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill102M : BulletSkillBase
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SkillBase skillBase;
+        if (SkillManager.Instance.skillDic.TryGetValue(102, out skillBase))
+        {
+            if (skillBase != null)
+            {
+                damage = skillBase.SkillObj.GetComponent<Skill102>().damage;
+                skillTime = skillBase.SkillObj.GetComponent<Skill102>().skillTime;
+                skillPene = skillBase.SkillObj.GetComponent<Skill102>().skillPene;
+                transform.localScale = skillBase.SkillObj.transform.localScale;
+            }
+        }
+        
+        Invoke("SelfDestory", skillTime);
+    }
+
+    private void SelfDestory()
+    {
+        Destroy(gameObject);
+    }
+    private void OnTriggerEnter2D(Collider2D obj)
+    {
+        if (obj.gameObject.CompareTag("Enemy"))
+        {
+            --skillPene;
+            if (skillPene == 0)
+            {
+                SelfDestory();
+            }
+        }
+    }
+}
